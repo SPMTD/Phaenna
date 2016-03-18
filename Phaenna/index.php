552432ecda2fc32e54e780ff84886a15da8686ae
Vorzious <tdvwit@gmail.com>
2016-03-18 11:55:36 +0100

    Navigation Edit
    Includes Features:
    
    Empty Index.php.
    Added Button myMood for navigation.

--- Phaenna/index.php
+++ Phaenna/index.php
@@ -2,25 +2,12 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <title>Phaenna Prototype 1.0</title>
-        <link rel="stylesheet" href="css/master.css">
-
+        <title>Phaenna</title>
     </head>
     <body>
 
-    <div id="container">
-        <button id = "btn_unhappy" name = "btn_unhappy">Unhappy</button>
-        <button id = "btn_neutral" name = "btn_neutral">Neutral</button>
-        <button id = "btn_happy" name = "btn_happy">Happy</button>
+    <a href="myMood.php"><button>My Mood</button></a>
 
 
-<!--        <div class="musicPlayerDiv">-->
-<!--            <audio id = "musicPlayer" autoplay = "true" controls prelod="none">-->
-<!--                <source id="mp3" src="audio/neutral_1.mp3" type="audio/mp3" />-->
-<!--            </audio>-->
-<!--        </div>-->
-    </div>
-
-    <script type="text/javascript" src="js/scripts.js"></script>
     </body>
 </html>
